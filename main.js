09086459274330c31cce33f539b63da93dc0fab1
AndiWatkins <andiwatkins22@gmail.com>
2026-05-13 20:50:19 -0700

    completed

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ var adjective1 = prompt('Provide an adjective.');
 var adjective2 = prompt('Provide another adjective.');
 var adverb = prompt('Provide an adverb.');
 
+let adLib = "There once was a " + adjective1 + "man who lived a " + adjective2 + "life and " + adverb + "worked at his job."
 // Write your code below
